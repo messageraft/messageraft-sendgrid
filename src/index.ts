b1449a0af96a93cd6f8734813cfce1b1a4ba3885
Petros Kyriakou <9pitops@gmail.com>
2021-08-30 14:53:00 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// import {
+//   EmailOptions,
+//   EmailProvider,
+//   Instantiable,
+//   Provider,
+//   ProviderName,
+//   ProviderType,
+//   SmsOptions,
+//   SmsProvider,
+// } from '@messageraft/common'
+
+import { EmailProvider } from '../../common'
+
+export class MessageRaftSendGridProvider extends EmailProvider {}
